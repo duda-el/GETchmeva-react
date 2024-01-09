7466960ba898644c1190d6dfa11f1902616ccba9
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-01-10 01:07:35 +0400

    Added HowItWorks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home"
+import HowItWorks from "./components/HowItWorks";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <div className="flex flex-col">
+        <Home />
+        <HowItWorks/>
+      </div>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import iphone from "../assets/iPhone.svg"
-import appstore from "../assets/AppSotore.svg"
-import android from "../assets/Android.svg"
+import appstore from "../assets/tempAppstore.svg"
+import android from "../assets/tempAppstore2.svg"
 
 export default function Home(){
   return(
@@ -9,7 +9,7 @@ export default function Home(){
       <div className="hero">
         <div className="md:px-20">
           <h1 className="custom-heading " ><span className="text-primary">GETchmeva </span>your personal<br  /> grocery assistant</h1>
-          <p className="mt-5 text-1.5xl">Available soon on these platforms!</p>
+          <p className="mt-5 text-1.5xl text-black">Available soon on these platforms!</p>
           <div className="download">
             <img src={appstore} alt="" className="md:w-44 md:h-auto sm:w-40 sm:h-auto cursor-pointer" />
             <img src={android} alt="" className="md:w-44 md:h-auto sm:w-40 sm:h-auto cursor-pointer" />
