dac57c433db665db9ede2152ca9f68763442fbeb
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-01-09 22:48:23 +0400

    Added particles for bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,8 +11,8 @@ export default function Home(){
           <h1 className="custom-heading " ><span className="text-primary">GETchmeva </span>your personal<br  /> grocery assistant</h1>
           <p className="mt-5 text-1.5xl">Available soon on these platforms!</p>
           <div className="download">
-            <img src={appstore} alt="" className="md:w-44 md:h-auto sm:w-40 sm:h-auto" />
-            <img src={android} alt="" className="md:w-44 md:h-auto sm:w-40 sm:h-auto" />
+            <img src={appstore} alt="" className="md:w-44 md:h-auto sm:w-40 sm:h-auto cursor-pointer" />
+            <img src={android} alt="" className="md:w-44 md:h-auto sm:w-40 sm:h-auto cursor-pointer" />
           </div>
         </div>
         <img src={iphone} alt="" className="levitating-img" />
